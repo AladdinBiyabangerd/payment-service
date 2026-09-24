5c7b6829ccbbaeae2f73ee476cdcf28caaed80ea
Aladdin <aladdin19.11.21@gmail.com>
2026-09-24 19:46:51 +0400

    issue

--- src/main/java/az/aladdin/paymentservice2/controller/PaymentController.java
+++ src/main/java/az/aladdin/paymentservice2/controller/PaymentController.java
@@ -22,7 +22,13 @@ public class PaymentControllerr {
     // api/v1/payments?userName=Ali
     @PostMapping()
     public String pay(@RequestHeader("Accept-Language")String lang, @RequestParam String userName, @RequestParam BigDecimal amount) {
+     asd
         return paymentService.pay(userName, amount);
     }
+sad
+
+
+
+
 
 }
