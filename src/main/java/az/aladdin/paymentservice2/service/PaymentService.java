62d9fcd35d93e1ba6e0e2a8196c4efca8c218581
Aladdin <aladdin19.11.21@gmail.com>
2026-09-24 20:32:25 +0400

    For conflict

--- src/main/java/az/aladdin/paymentservice2/service/PaymentService.java
+++ src/main/java/az/aladdin/paymentservice2/service/PaymentService.java
@@ -13,6 +13,7 @@ public class PaymentService {
 
 //    private final PaymentRepository paymentRepository;
 
+
     public String pay(String userName, BigDecimal amount) {
         var paymentEntity = PaymentEntity.builder()
                 .name(userName)
