bd62ae01c0b1df15f3777572b075078aeff3dcf9
Aladdin <aladdin19.11.21@gmail.com>
2026-09-26 12:34:40 +0400

    Optimized project

--- src/main/java/az/aladdin/paymentservice2/service/PaymentService.java
+++ src/main/java/az/aladdin/paymentservice2/service/PaymentService.java
@@ -1,32 +1,11 @@
 package az.aladdin.paymentservice2.service;
 
-import az.aladdin.paymentservice2.dao.entity.PaymentEntity;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.math.BigDecimal;
 
 @Service
 @RequiredArgsConstructor
 public class PaymentService {
 
 
-//    private final PaymentRepository paymentRepository;
-
-
-    public String pay(String userName, BigDecimal amount) {
-        var paymentEntity = PaymentEntity.builder()
-                .name(userName)
-                .amount(amount)
-                .build();
-
-//        var paymentEntity = PaymentEntity.builder()
-//                .name(userName)
-//                .amount(amount)
-//                .build();
-
-      //  PaymentEntity save = paymentRepository.save(paymentEntity);
-
-        return "Username: " + userName + " amount " + amount;
-    }
 }
